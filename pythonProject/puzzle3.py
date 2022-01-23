b1fe734b0766dedfaad9829d7f0a8d225f01fc6e
Andrei Zhernakov <a.s.jernakov@gmail.com>
2022-01-23 15:04:41 +0300

    some reformat in puzzles and puzzle15 added

--- pythonProject/puzzle3.py
+++ pythonProject/puzzle3.py
@@ -1,6 +1,5 @@
 # По заданному номеру дня недели вывести его название
 
 listDays = ['Monday', 'Tuesday', 'Wedneday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-print('Input week day number\n')
-n = int(input())
+n = int(input('Input week day number\n'))
 print(listDays[n - 1])
