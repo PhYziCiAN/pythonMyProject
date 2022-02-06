f944e5f6f2e40ea414449a358821c0ce19441a3b
Andrei Zhernakov <a.s.jernakov@gmail.com>
2022-02-06 11:21:37 +0300

    shure_solved

--- pythonProject/shure_eq/shure.py
+++ pythonProject/shure_eq/shure.py
@@ -1,4 +1,11 @@
 
-with open(file='shure50.txt', mode='r', newline='\n') as f:
-    lines = f.readline()
-    print(lines)
+h = open(file='/home/phyzician/LearnGB/pythonMyProject/pythonProject/shure_eq/shure50_1.feq', mode='w')
+with open(file='/home/phyzician/LearnGB/pythonMyProject/pythonProject/shure_eq/shure50.feq', mode='r') as f:
+    list = f.readlines()
+for i in list:
+    a = str(int(i)+1)
+    h.write(a)
+    h.write('\n')
+h.close()
+
+    
